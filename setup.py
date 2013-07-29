42ea4b53c3be67e944bb96a03763df87dfdf84a5
Paul <paul@benchline.org>
2013-07-29 14:57:33 -0600

    Fixed issues with setup.py

--- setup.py
+++ setup.py
@@ -16,14 +16,14 @@ with open('README.rst') as rm_file:
     long_description = rm_file.read()
 
 setup(name='byu-ws-sdk-python',
-      version='0.9.2',
+      version='0.9.3',
       description='A Python SDK for calling BYU REST web services.',
       long_description=long_description,
       author='Paul Eden',
       author_email='paul_eden@byu.edu',
       url='https://github.com/byu-oit-core-appeng/byu-ws-sdk-python',
       packages=['byu_ws_sdk'],
-      package_data={'byu_ws_sdk': ['README*', 'LICENSE']},
+      data_files=[('', ['README.md', 'README.rst', 'LICENSE'])],
       license="MIT",
       requires=['requests (>=0.14.1, <=0.14.2)', 'simplejson', 'decorator']
       )
